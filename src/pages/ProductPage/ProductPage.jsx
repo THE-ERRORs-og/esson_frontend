5044d1248b99a39a899eb839c2c7a40a7cb1de4a
Shivam Varshney <12111043@nitkkr.ac.in>
2024-10-14 19:17:26 +0530

    Product Page Chnages"

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Corousel from './Components/Corousel'
+import CoroselSideBar from './Components/CoroselSideBar'
+import AboutProduct from './Components/AboutProduct'
 
 const ProductPage = () => {
   return (
     <div>
-      <Corousel />
+      <div className='flex '>
+        <div className='w-[100%]'>
+          <Corousel className="w-[60%]"/>
+          <AboutProduct />
+        </div>
+        <CoroselSideBar className=""/>
+      </div>
+      
+
     </div>
   )
 }
