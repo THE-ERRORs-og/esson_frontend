dbbfff8266d5cf815ebe7c8aba5475b0ab7bdaed
Govind Ji <jigovind556@gmail.com>
2024-11-08 00:40:54 +0530

    changed images in the hero section of landing page

--- src/pages/Landing/Hero.jsx
+++ src/pages/Landing/Hero.jsx
@@ -1,18 +1,21 @@
 
 import { Button } from "../../components/ui/button";
 import React from "react";
-import { headphone,watch, b1 } from "../../assets/images";
+import { craft_bag, watch,boss_cup_white,pizza_box_single, b1 } from "../../assets/images";
 
 const Hero = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4 md:p-8 bg-gray-100">
       {/* Main Promotion */}
-      <div className="col-span-1 md:col-span-2 rounded-lg shadow-lg relative  bg-cover bg-center" style={{ backgroundImage: `url(${headphone})` }}>
+      <div
+        className="col-span-1 md:col-span-2 rounded-lg shadow-lg relative  bg-cover bg-center"
+        style={{ backgroundImage: `url(${craft_bag})` }}
+      >
         <div className="flex flex-col space-y-12 md:space-y-64">
           <div className="w-3/4 md:w-1/2 flex flex-col justify-center space-y-4 p-6 mt-4 md:mt-10 ml-4 md:ml-14">
-            <span className="bg-blue-500 w-24 text-white px-4 py-1 text-sm font-semibold rounded-full">
+            {/* <span className="bg-blue-500 w-24 text-white px-4 py-1 text-sm font-semibold rounded-full">
               SALE $100
-            </span>
+            </span> */}
 
             {/* Product Text */}
             <div>
@@ -28,7 +31,9 @@ const Hero = () => {
             {/* <button className="bg-gradient-to-r from-red-500 to-orange-500 font-bold w-1/3 md:w-1/4 text-white md:text-xs md:py-3 px-4 rounded-3xl hover:bg-pink-600">
               SHOW NOW
             </button> */}
-            <Button className= "bg-gradient-to-r from-red-500 to-orange-500 font-bold w-1/2 md:w-1/2 text-white text-xs md:py-3 px-4 rounded-3xl hover:bg-pink-600">Shop Now</Button>
+            <Button className="bg-gradient-to-r from-red-500 to-orange-500  w-1/2 md:w-1/2 text-white text-lg md:py-3 px-4 rounded-3xl hover:bg-pink-600">
+              Reqest a Quote
+            </Button>
           </div>
 
           {/* Carousel Dots */}
@@ -42,12 +47,15 @@ const Hero = () => {
       {/* Side Promotions */}
       <div className="flex justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-6 relative ">
         {/* Microsoft Headphone */}
-        <div className="rounded-lg w-full shadow-lg relative bg-cover bg-center h-52 md:h-full"  style={{ backgroundImage: `url(${b1})` }}>
+        <div
+          className="rounded-lg w-full shadow-lg relative bg-cover bg-center h-52 md:h-full"
+          style={{ backgroundImage: `url(${boss_cup_white})` }}
+        >
           <div className="flex flex-col h-full justify-center">
             <div className="w-3/4 md:w-1/2 flex flex-col justify-center space-y-4 p-6 ml-4 md:ml-14">
               <h2 className="text-xl md:text-2xl font-thin text-black">
-                Microsoft<br />{" "}
-                <span className="font-bold">Watch</span>
+                Microsoft
+                <br /> <span className="font-bold">Watch</span>
               </h2>
               <p className="text-black mt-2 text-sm md:text-base">
                 From <span className="text-red-500">$69.99</span>
@@ -62,12 +70,15 @@ const Hero = () => {
         </div>
 
         {/* iPhone X Collection */}
-        <div className="rounded-lg w-full shadow-lg relative bg-cover bg-center h-52 md:h-full" style={{ backgroundImage: `url(${watch})` }}>
+        <div
+          className="rounded-lg w-full shadow-lg relative bg-cover bg-center h-52 md:h-full"
+          style={{ backgroundImage: `url(${pizza_box_single})` }}
+        >
           <div className="flex flex-col h-full justify-center">
             <div className="w-3/4 md:w-1/2 flex flex-col justify-center space-y-4 p-6 ml-4 md:ml-14">
               <h2 className="text-xl md:text-2xl font-thin text-black">
-                Apple<br />{" "}
-                <span className="font-bold">VR</span>
+                Apple
+                <br /> <span className="font-bold">VR</span>
               </h2>
               <p className="text-black mt-2 text-sm md:text-base">
                 From <span className="text-red-500">$69.99</span>
